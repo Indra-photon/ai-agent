e55ed45159a49dd1cf34e7f347eefa1c73de03e7
Indranil Maiti <indranilmaiti1@gmail.com>
2025-06-07 19:33:30 +0200

    crewai-youtube-analyzer

---
+++ crewAI_web_search/youtubeanalyzer.py
@@ -0,0 +1,55 @@
+from crewai import Agent, Task, LLM
+import os
+from dotenv import load_dotenv
+from crewai import Crew, Process
+from crewai_tools import SerperDevTool, YoutubeVideoSearchTool, YoutubeChannelSearchTool
+
+load_dotenv()
+
+default_llm = LLM(
+    model="openai/meta-llama/Meta-Llama-3.1-70B-Instruct",
+    base_url="https://api.studio.nebius.com/v1/",
+    api_key=os.getenv("NEBIUS_API_KEY")
+)
+
+researcher = Agent(
+    role="youtube video details e.g. tiltle, description, tags, channel analysis",
+    goal="Uncover the hidden tags, title, description, and channel analysis of YouTube videos in a specific niche.",    
+    backstory="You are an expert in YouTube video analysis, specializing in extracting detailed metadata and insights from videos.",
+    tools=[YoutubeVideoSearchTool(), YoutubeChannelSearchTool()],
+    llm=default_llm
+)
+
+writer = Agent(
+    role="Report Writer",
+    goal="Write a compelling report on the YouTube video analysis findings focusing on numbers views tags title, description, and channel " \
+    "analysis. Try to find the most popular videos in the niche and its success factors. write in a professional tone, to the point, "
+    "and with a focus on actionable insights. Use bullet points for clarity. focus on numbers.",
+    verbose=True,
+    llm=default_llm,
+    backstory="You excel at translating complex data into readable content, focusing on actionable insights and clarity.",
+    tools=[]
+)
+
+research_task = Task(
+    description="Research the successful YouTube videos in the niche of AI agents, focusing on their tags, titles, descriptions, and channel analysis.",
+    expected_output="A report summarizing key insights from the YouTube videos, including tags, titles, descriptions, and channel analysis.",
+    agent=researcher  # Assign the researcher agent
+)
+
+write_task = Task(
+    description="Write a detailed report based on the YouTube video analysis findings, focusing on numbers views tags title, description, and channel analysis.",
+    expected_output="An engaging report on the YouTube video analysis findings, highlighting key metrics and insights.",
+    agent=writer,  # Assign the writer agent
+    context=[research_task]  # Use research task output as context
+)
+
+crew = Crew(
+    agents=[researcher, writer],
+    tasks=[research_task, write_task],
+    process=Process.sequential  # The agents work in sequence
+)
+
+# Execute the crew and capture the result
+result = crew.kickoff(inputs={"topic": "AI agents in YouTube videos"})
+#print(result)
